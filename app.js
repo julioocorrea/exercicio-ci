695d8e840b72fe5b27b305d58f10137be50ec43d
Julio Correa <julioo_correa21@hotmail.com>
2025-11-18 20:54:25 -0300

    teste 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Olá comunidade DevOps!");
+  res.status(200).send("Olá comunidadeeeee DevOps!");
 });
 
 module.exports = app; 
