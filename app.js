56f320cd9155fe16cec7f50602966ad5bd01b189
Julio Correa <julioo_correa21@hotmail.com>
2025-11-18 21:23:36 -0300

    teste

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Olá planeta2 DevOps!");
+  res.status(200).send("Olá planeta3 DevOps!");
 });
 
 module.exports = app; 
